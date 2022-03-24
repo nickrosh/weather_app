d72da6e1b6ecd29f3baadcef262ed55d92d4e518
Nick Roshdieh <nickroshd@gmail.com>
2022-03-23 23:28:46 -0400

    Added Navbar

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+            <div className="container">
+                <h1 className="display-5 text-white">Weather@Nick</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
