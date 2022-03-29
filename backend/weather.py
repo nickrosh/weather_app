ad23a527867f06417ebb8d3cd2217cfdb2186d11
Nick Roshdieh <nickroshd@gmail.com>
2022-03-29 00:25:44 -0400

    changed API to use imperial units

--- backend/weather.py
+++ backend/weather.py
@@ -3,6 +3,6 @@ from backend.config import settings
 
 
 async def weather_request():
-    API_ENDPOINT = f'https://api.openweathermap.org/data/2.5/onecall?lat={settings.LATITUDE}&lon={settings.LONGITUDE}&appid={settings.API_KEY}'
+    API_ENDPOINT = f'https://api.openweathermap.org/data/2.5/onecall?lat={settings.LATITUDE}&lon={settings.LONGITUDE}&appid={settings.API_KEY}&units=imperial'
     response = requests.get(API_ENDPOINT)
     return response
--- frontend/src/components/Current.js
+++ frontend/src/components/Current.js
@@ -3,11 +3,10 @@
 const Current = ({data}) => {
 
     return (
-    <section className="bg-light p-5">
+    <section className="bg-light p-5 text-center">
         <div className="container">
-            <p>Temperature is {data.temp}</p>
-            <p>Humidity is {data.humidity}</p>
-            <p>Pressure is {data.pressure}</p>
+            <h3>Temperature is <strong>{data.temp}</strong></h3>
+            <h3>Humidity is <strong>{data.humidity}</strong></h3>
 
             {/* for some reason, this is throwing an error */}
             {/* <p>Weather is {data.weather[0].main}</p>
