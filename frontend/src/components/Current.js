658f7d054044231413d20c0502ca7fe12ba36a25
Nick Roshdieh <nickroshd@gmail.com>
2022-03-29 01:15:29 -0400

    added icon and descriptioni for current weather

--- frontend/src/components/Current.js
+++ frontend/src/components/Current.js
@@ -8,11 +8,11 @@ const Current = ({data}) => {
             <h3>Temperature is <strong>{data?.main?.temp}&deg;F</strong></h3>
             <h3>Humidity is <strong>{data?.main?.humidity}%</strong></h3>
 
-            {/* {data.weather.map(condition =>
-            <div key={condition.id}>
-                <img src={getIconUrl(condition.icon)} alt={condition.main}/> {condition.main} {condition.description}
-            </div>)
-            } */}
+            {data?.weather?.map(condition =>
+            <h3 key={condition.id}>
+                <img src={getIconUrl(condition.icon)} alt={condition.main} className="image-fluid"/> {condition.main}: {condition.description}
+            </h3>)
+            }
       </div>
     </section>
     )
